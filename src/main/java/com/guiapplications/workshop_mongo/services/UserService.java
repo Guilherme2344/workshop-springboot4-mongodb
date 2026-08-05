9d6de89f704076f91bbadbbe2abb3fc6371d34da
Guilherme2344 <guilhermecsilva2.alternative@gmail.com>
2026-08-05 12:05:12 -0300

    adding users with POST

--- src/main/java/com/guiapplications/workshop_mongo/services/UserService.java
+++ src/main/java/com/guiapplications/workshop_mongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.guiapplications.workshop_mongo.domain.User;
+import com.guiapplications.workshop_mongo.dto.UserDTO;
 import com.guiapplications.workshop_mongo.repository.UserRepository;
 import com.guiapplications.workshop_mongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
 	public User findById(String id) {
 		Optional<User> obj = userRepository.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	public User fromDTO(UserDTO objDto) {
+		return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
 	}
 }

4e14b99c614baf76e6f6a2a3a5273ae12b94e740
Guilherme2344 <guilhermecsilva2.alternative@gmail.com>
2026-08-05 17:59:10 -0300

    Query with lots of criteria

--- src/main/java/com/guiapplications/workshop_mongo/resources/PostResource.java
+++ src/main/java/com/guiapplications/workshop_mongo/resources/PostResource.java
@@ -1,5 +1,6 @@
 package com.guiapplications.workshop_mongo.resources;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,9 +28,22 @@ public class PostResource {
 	}
 	
 	@GetMapping(value = "/titlesearch")
-	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "") String text){
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue = "") String text){
 		text = URL.decodeParam(text);
 		List<Post> list = postService.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(defaultValue = "") String text,
+			@RequestParam(defaultValue = "") String minDate,
+			@RequestParam(defaultValue = "") String maxDate){
+		
+		text = URL.decodeParam(text);
+		LocalDateTime min = URL.convertDate(minDate, LocalDateTime.of(1970, 1, 1, 0, 0));
+		LocalDateTime max = URL.convertDate(maxDate, LocalDateTime.now());
+		List<Post> list = postService.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/guiapplications/workshop_mongo/resources/URL.java
+++ src/main/java/com/guiapplications/workshop_mongo/resources/URL.java
@@ -2,6 +2,10 @@ package com.guiapplications.workshop_mongo.resources;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 
 public class URL {
 	
@@ -10,6 +14,15 @@ public class URL {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
+		}
+	}
+	
+	public static LocalDateTime convertDate(String textDate, LocalDateTime defaultValue) {
+		try {
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+			return LocalDate.parse(textDate, formatter).atTime(23, 59, 59);
+		} catch (DateTimeParseException e) {
+			return defaultValue;
 		}
 	}
 }

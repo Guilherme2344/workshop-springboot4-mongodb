8dd5c90d50d1d68fd039321d4d7ce38f0cf37686
Guilherme2344 <guilhermecsilva2.alternative@gmail.com>
2026-08-05 14:33:31 -0300

    Referencing user posts

--- src/main/java/com/guiapplications/workshop_mongo/config/TestConfig.java
+++ src/main/java/com/guiapplications/workshop_mongo/config/TestConfig.java
@@ -43,6 +43,9 @@ public class TestConfig implements CommandLineRunner {
 		Post p2 = new Post(null, LocalDate.parse("23/03/2018", formatter), "Partiu comer", "Vou comer", new AuthorDTO(maria));
 		
 		postRepository.saveAll(Arrays.asList(p1, p2));
+	
+		maria.getPosts().addAll(Arrays.asList(p1, p2));
+		userRepository.save(maria);
 	}
 
 }
